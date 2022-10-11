fc318f6fa6e01cb6936f27e6733eabf2665b24f2
Ricky Indra Gunawan <rickyindra006@gmail.com>
2022-10-11 19:19:28 +0700

    add AppBar.tsx, moving index.css to app/styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,14 @@
+import { AppBar } from "./AppBar";
+import "./styles/index.css";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div className="fixed top-0 left-1/2 z-10 h-16 w-full max-w-screen-2xl -translate-x-1/2">
+        <AppBar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
